6ff9b5cf9d780416fb26c36e4b2475d1f3c79f0e
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-05-29 17:20:35 +0100

    Criação da activity editar suspeito

---
+++ app/src/main/java/pt/ipg/covid19/activity_sus_inf_editar.java
@@ -0,0 +1,14 @@
+package pt.ipg.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_sus_inf_editar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sus_inf_editar);
+    }
+}
