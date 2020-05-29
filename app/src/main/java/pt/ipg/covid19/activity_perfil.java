7c615ad2407da94b835a15b7cec91990994318a4
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-05-29 17:04:42 +0100

    Criação da activity editar perfil

--- app/src/main/java/pt/ipg/covid19/activity_perfil.java
+++ app/src/main/java/pt/ipg/covid19/activity_perfil.java
@@ -2,7 +2,9 @@ package pt.ipg.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_perfil extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class activity_perfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+    }
+
+    public void onClickEditarPerfil(View view){
+        Intent IntentEditarPerfil = new Intent(this, activity_perfil_editar.class);
+        startActivity(IntentEditarPerfil);
     }
 }
---
+++ app/src/main/java/pt/ipg/covid19/activity_perfil_editar.java
@@ -0,0 +1,18 @@
+package pt.ipg.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class activity_perfil_editar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_editar);
+    }
+
+
+}
