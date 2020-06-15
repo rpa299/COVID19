ff82cab076d020393c6f985a2c839057e0a3d486
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-15 17:58:12 +0100

    pequenas alterações no design do botão confirmar e cancelar e adição de suporte de data na atividade editar perfil

--- app/src/main/java/pt/ipg/covid19/activity_perfil_editar.java
+++ app/src/main/java/pt/ipg/covid19/activity_perfil_editar.java
@@ -2,15 +2,25 @@ package pt.ipg.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.time.Year;
+import java.util.Calendar;
 
 public class activity_perfil_editar extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
-
+    TextView dataNascimento;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +39,31 @@ public class activity_perfil_editar extends AppCompatActivity implements Adapter
         spinnerSangue.setOnItemSelectedListener(this);
     }
 
+    public void escolherData(View view){
+        dataNascimento = findViewById(R.id.textViewDataNascimento);
+
+        final Calendar calendar = Calendar.getInstance();
+        int YEAR = calendar.get(Calendar.YEAR);
+        int MONTH = calendar.get(Calendar.MONTH);
+        int DATE = calendar.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.YEAR, year);
+                calendar1.set(Calendar.MONTH, month);
+                calendar1.set(Calendar.DATE, date);
+
+                CharSequence dateCharSequence = DateFormat.format("dd/MM/yyyy",calendar1);
+
+                dataNascimento.setText(dateCharSequence);
+
+            }
+        }, YEAR,MONTH,DATE);
+        datePickerDialog.show();
+    }
+
     public void onClickPerfilEditar(View view){
         Intent IntentPerfilEditar = new Intent(this, activity_perfil_editar.class);
         startActivity(IntentPerfilEditar);
@@ -36,18 +71,35 @@ public class activity_perfil_editar extends AppCompatActivity implements Adapter
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        switch(parent.getId()){
-            case R.id.spinnerSexoEditPerfil:
-                String sexo = parent.getItemAtPosition(position).toString();
-                break;
-            case R.id.spinnerTipoSangueEditPerfil:
-                String sangue = parent.getItemAtPosition(position).toString();
-                break;
-        }
+
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    class SpinnerSexoPerfil implements AdapterView.OnItemSelectedListener{
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String sexo = parent.getItemAtPosition(position).toString();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+
+    class SpinnerSanguePerfil implements AdapterView.OnItemSelectedListener{
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String sangue = parent.getItemAtPosition(position).toString();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
     }
 }
