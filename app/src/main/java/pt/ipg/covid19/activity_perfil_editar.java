f67c3c7c0ade76f37f4a7c2b166f8aadaaf5cce4
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-15 11:05:21 +0100

    Criação de um spinner para selecionar o sexo na atividade editar perfil

--- app/src/main/java/pt/ipg/covid19/activity_perfil_editar.java
+++ app/src/main/java/pt/ipg/covid19/activity_perfil_editar.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class activity_perfil_editar extends AppCompatActivity {
+public class activity_perfil_editar extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_editar);
+
+        Spinner spinner = findViewById(R.id.spinnerSexoEditPerfil);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.SexoPerfil,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     public void onClickPerfilEditar(View view){
@@ -19,4 +28,13 @@ public class activity_perfil_editar extends AppCompatActivity {
         startActivity(IntentPerfilEditar);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String sexo = parent.getItemAtPosition(position).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
