84b062739791ed0b25093c82159cef0e5a07d113
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-22 15:17:50 +0100

    Codigo da atividade Sintomas feito.

--- app/src/main/java/pt/ipg/covid19/activity_sintomas.java
+++ app/src/main/java/pt/ipg/covid19/activity_sintomas.java
@@ -2,7 +2,14 @@ package pt.ipg.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class activity_sintomas extends AppCompatActivity {
 
@@ -10,5 +17,30 @@ public class activity_sintomas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sintomas);
+    }
+
+    public void escolherData(View view){
+        final TextView dataSintoma = findViewById(R.id.textViewSintomaDataAddView);
+
+        final Calendar calendar = Calendar.getInstance();
+        int YEAR = calendar.get(Calendar.YEAR);
+        int MONTH = calendar.get(Calendar.MONTH);
+        int DATE = calendar.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.YEAR, year);
+                calendar1.set(Calendar.MONTH, month);
+                calendar1.set(Calendar.DATE, date);
+
+                CharSequence dateCharSequence = DateFormat.format("dd/MM/yyyy",calendar1);
+
+                dataSintoma.setText(dateCharSequence);
+
+            }
+        }, YEAR,MONTH,DATE);
+        datePickerDialog.show();
     }
 }
