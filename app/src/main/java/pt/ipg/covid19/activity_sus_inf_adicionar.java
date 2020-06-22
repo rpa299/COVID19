35a2d14eb5366c734d943ed14da77aca83945b49
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-22 15:46:58 +0100

    Código para a atividade adicionar Suspeito

--- app/src/main/java/pt/ipg/covid19/activity_sus_inf_adicionar.java
+++ app/src/main/java/pt/ipg/covid19/activity_sus_inf_adicionar.java
@@ -2,7 +2,15 @@ package pt.ipg.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class activity_sus_inf_adicionar extends AppCompatActivity {
 
@@ -10,5 +18,64 @@ public class activity_sus_inf_adicionar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sus_inf_adicionar);
+    }
+
+    public void escolherDataNascimento(View view){
+        final TextView dataSintoma = findViewById(R.id.textViewDataNascimentoSusAdd);
+
+        final Calendar calendar = Calendar.getInstance();
+        int YEAR = calendar.get(Calendar.YEAR);
+        int MONTH = calendar.get(Calendar.MONTH);
+        int DATE = calendar.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.YEAR, year);
+                calendar1.set(Calendar.MONTH, month);
+                calendar1.set(Calendar.DATE, date);
+
+                CharSequence dateCharSequence = DateFormat.format("dd/MM/yyyy",calendar1);
+
+                dataSintoma.setText(dateCharSequence);
+
+            }
+        }, YEAR,MONTH,DATE);
+        datePickerDialog.show();
+    }
+
+    public void escolherDataInf(View view){
+        final TextView dataSintoma = findViewById(R.id.textViewDataInfSusAdd);
+
+        final Calendar calendar = Calendar.getInstance();
+        int YEAR = calendar.get(Calendar.YEAR);
+        int MONTH = calendar.get(Calendar.MONTH);
+        int DATE = calendar.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.YEAR, year);
+                calendar1.set(Calendar.MONTH, month);
+                calendar1.set(Calendar.DATE, date);
+
+                CharSequence dateCharSequence = DateFormat.format("dd/MM/yyyy",calendar1);
+
+                dataSintoma.setText(dateCharSequence);
+
+            }
+        }, YEAR,MONTH,DATE);
+        datePickerDialog.show();
+    }
+
+    public void Guardar(View view){
+
+    }
+
+    public void cancelar(View view){
+        finish();
+        Toast.makeText(this, ("Cancelado"),Toast.LENGTH_LONG).show();
     }
 }
