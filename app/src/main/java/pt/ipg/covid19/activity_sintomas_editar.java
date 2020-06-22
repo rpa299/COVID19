d45c642aff3d0c273fe40e6815b22eed359c1c97
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-22 15:15:16 +0100

    Codigo da atividade Sintomas Edit feito.

--- app/src/main/java/pt/ipg/covid19/activity_sintomas_editar.java
+++ app/src/main/java/pt/ipg/covid19/activity_sintomas_editar.java
@@ -2,11 +2,18 @@ package pt.ipg.covid19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class activity_sintomas_editar extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -21,41 +28,75 @@ public class activity_sintomas_editar extends AppCompatActivity implements Adapt
         spinnerDoresCabeca.setAdapter(adapterDoresCabeca);
         spinnerDoresCabeca.setOnItemSelectedListener(this);
 
-        Spinner spinnerDoresMusculares = findViewById(R.id.spinnerDoresMuscularesAdd);
+        Spinner spinnerDoresMusculares = findViewById(R.id.spinnerDoresMuscularesEdit);
         ArrayAdapter<CharSequence> adapterDoresMusculares = ArrayAdapter.createFromResource(this, R.array.sintomas1, android.R.layout.simple_spinner_item);
         adapterDoresMusculares.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerDoresMusculares.setAdapter(adapterDoresMusculares);
         spinnerDoresMusculares.setOnItemSelectedListener(this);
 
-        Spinner spinnerCansaco = findViewById(R.id.spinnerCansacoAdd);
+        Spinner spinnerCansaco = findViewById(R.id.spinnerCansacoEdit);
         ArrayAdapter<CharSequence> adapterCansaco = ArrayAdapter.createFromResource(this, R.array.sintomas1, android.R.layout.simple_spinner_item);
         adapterCansaco.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCansaco.setAdapter(adapterCansaco);
         spinnerCansaco.setOnItemSelectedListener(this);
 
-        Spinner spinnerDoresGarganta = findViewById(R.id.spinnerDoresGargantaAdd);
+        Spinner spinnerDoresGarganta = findViewById(R.id.spinnerDoresGargantaEdit);
         ArrayAdapter<CharSequence> adapterDoresGarganta = ArrayAdapter.createFromResource(this, R.array.sintomas1, android.R.layout.simple_spinner_item);
         adapterDoresGarganta.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerDoresGarganta.setAdapter(adapterDoresGarganta);
         spinnerDoresGarganta.setOnItemSelectedListener(this);
 
-        Spinner spinnerTosse = findViewById(R.id.spinnerTosseAdd);
+        Spinner spinnerTosse = findViewById(R.id.spinnerTosseEdit);
         ArrayAdapter<CharSequence> adapterTosse = ArrayAdapter.createFromResource(this, R.array.sintomas1, android.R.layout.simple_spinner_item);
         adapterTosse.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerTosse.setAdapter(adapterTosse);
         spinnerTosse.setOnItemSelectedListener(this);
 
-        Spinner spinnerRespiracao = findViewById(R.id.spinnerRespiracaoAdd);
+        Spinner spinnerRespiracao = findViewById(R.id.spinnerRespiracaoEdit);
         ArrayAdapter<CharSequence> adapterRespiracao = ArrayAdapter.createFromResource(this, R.array.sintomas2, android.R.layout.simple_spinner_item);
         adapterRespiracao.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerRespiracao.setAdapter(adapterRespiracao);
         spinnerRespiracao.setOnItemSelectedListener(this);
 
-        Spinner spinnerCorrimentoNasal = findViewById(R.id.spinnerCorrimentoNasalAdd);
+        Spinner spinnerCorrimentoNasal = findViewById(R.id.spinnerCorrimentoNasalEdit);
         ArrayAdapter<CharSequence> adapterCorrimentoNasal = ArrayAdapter.createFromResource(this, R.array.sintomas1, android.R.layout.simple_spinner_item);
         adapterCorrimentoNasal.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCorrimentoNasal.setAdapter(adapterCorrimentoNasal);
         spinnerCorrimentoNasal.setOnItemSelectedListener(this);
+    }
+
+    public void escolherData(View view){
+        final TextView dataSintoma = findViewById(R.id.textViewSintomaDataEdit);
+
+        final Calendar calendar = Calendar.getInstance();
+        int YEAR = calendar.get(Calendar.YEAR);
+        int MONTH = calendar.get(Calendar.MONTH);
+        int DATE = calendar.get(Calendar.DATE);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int date) {
+                Calendar calendar1 = Calendar.getInstance();
+                calendar1.set(Calendar.YEAR, year);
+                calendar1.set(Calendar.MONTH, month);
+                calendar1.set(Calendar.DATE, date);
+
+                CharSequence dateCharSequence = DateFormat.format("dd/MM/yyyy",calendar1);
+
+                dataSintoma.setText(dateCharSequence);
+
+            }
+        }, YEAR,MONTH,DATE);
+        datePickerDialog.show();
+    }
+
+    public void Guardar(View view){
+
+    }
+
+    public void cancelar(View view){
+        finish();
+        Toast.makeText(this, ("Cancelado"),Toast.LENGTH_LONG).show();
     }
 
     @Override
