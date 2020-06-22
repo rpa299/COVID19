5a0daa1ae5e1c7f00da33d4c1c03e1f16c88c332
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-22 16:19:09 +0100

    Criação da classe perfil para ser utilizada na BD

---
+++ app/src/main/java/pt/ipg/covid19/Perfil.java
@@ -0,0 +1,58 @@
+package pt.ipg.covid19;
+
+public class Perfil {
+    private long id = -1;
+    private String dataNascimento;
+    private String sexo;
+    private int altura;
+    private float peso;
+    private String tipoSangue;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getTipoSangue() {
+        return tipoSangue;
+    }
+
+    public void setTipoSangue(String tipoSangue) {
+        this.tipoSangue = tipoSangue;
+    }
+}
