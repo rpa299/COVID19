ce495407ec9f81947335847e60a61ea3a571b2bb
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-23 17:39:46 +0100

    Criadas funções a ser usadas nos testes para fazer CRUD das tabelas

--- app/src/androidTest/java/pt/ipg/covid19/BdCovidTest.java
+++ app/src/androidTest/java/pt/ipg/covid19/BdCovidTest.java
@@ -39,5 +39,75 @@ public class BdCovidTest {
         return InstrumentationRegistry.getInstrumentation().getTargetContext();
     }
 
+    //funções para o perfil
+    private long inserePerfil(BdTablePerfil tabelaPerfil, Perfil perfil) {
+        long id = tabelaPerfil.insert(Converte.PerfilToContentValues(perfil));
+        assertNotEquals(-1, id);
+        return id;
+    }
 
+    private long inserePerfil(BdTablePerfil tabelaPerfil, String nome, String dataNascimento, String sexo, int altura, float peso, String tipoSangue) {
+
+        Perfil perfil = new Perfil();
+        perfil.setNome(nome);
+        perfil.setDataNascimento(dataNascimento);
+        perfil.setSexo(sexo);
+        perfil.setAltura(altura);
+        perfil.setPeso(peso);
+        perfil.setTipoSangue(tipoSangue);
+
+        return inserePerfil(tabelaPerfil, perfil);
+    }
+
+    //função para os sintomas
+    private long insereSintoma(SQLiteDatabase bdCovid, String data, String doresCabeca, String doresMusculares, String cansaco,
+                               String doresGarganta, String tosse, float temperatura, String respiracao, String corrimentoNasal,
+                               String nome, String dataNascimento, String sexo, int altura, float peso, String tipoSangue) {
+
+        BdTablePerfil tabelaPerfil = new BdTablePerfil(bdCovid);
+
+        long idPerfil = inserePerfil(tabelaPerfil, nome, dataNascimento, sexo, altura, peso, tipoSangue);
+
+        Sintoma sintoma = new Sintoma();
+        sintoma.setData(data);
+        sintoma.setDoresCabeca(doresCabeca);
+        sintoma.setDoresMusculares(doresMusculares);
+        sintoma.setCansaco(cansaco);
+        sintoma.setDoresGarganta(doresGarganta);
+        sintoma.setTosse(tosse);
+        sintoma.setTemperatura(temperatura);
+        sintoma.setRespiracao(respiracao);
+        sintoma.setCorrimentoNasal(corrimentoNasal);
+        sintoma.setIdPerfil(idPerfil);
+
+        BdTableSintoma tabelaSintoma = new BdTableSintoma(bdCovid);
+        long id = tabelaSintoma.insert(Converte.SintomaToContentValues(sintoma));
+        assertNotEquals(-1, id);
+
+        return  id;
+    }
+
+    //função para os suspeitos ou infetados
+    private long insereSusInf(SQLiteDatabase bdCovid, String nomeSusInf, String dataNascimentoSusInf, String dataInfecao,
+                               String nome, String dataNascimento, String sexo, int altura, float peso, String tipoSangue) {
+
+        BdTablePerfil tabelaPerfil = new BdTablePerfil(bdCovid);
+
+        long idPerfil = inserePerfil(tabelaPerfil, nome, dataNascimento, sexo, altura, peso, tipoSangue);
+
+        SusInf susInf = new SusInf();
+        susInf.setNomeSusInf(nomeSusInf);
+        susInf.setDataNascimento(dataNascimentoSusInf);
+        susInf.setDataInfecao(dataInfecao);
+        susInf.setIdPerfil(idPerfil);
+
+        BdTableSusInf tabelaSusInf = new BdTableSusInf(bdCovid);
+        long id = tabelaSusInf.insert(Converte.SusInfToContentValues(susInf));
+        assertNotEquals(-1, id);
+
+        return  id;
+    }
+
+    //testes para ler/alterar/inserir/eliminar Perfil
+    
 }
