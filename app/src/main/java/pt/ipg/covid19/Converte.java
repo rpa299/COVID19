9afae77404d6b6c5e944c968fc123bdff4cc725a
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-24 09:45:09 +0100

    Criadas funções cursor na classe converte para serem usadas posteriormente

--- app/src/main/java/pt/ipg/covid19/Converte.java
+++ app/src/main/java/pt/ipg/covid19/Converte.java
@@ -1,6 +1,7 @@
 package pt.ipg.covid19;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 
 public class Converte {
     //para a tabela sintoma
@@ -86,6 +87,52 @@ public class Converte {
         susInf.setDataInfecao(valores.getAsString(BdTableSusInf.CAMPO_DATA_INFECAO));
         susInf.setDataNascimento(valores.getAsString(BdTableSusInf.CAMPO_DATA_NASCIMENTO));
 
+        return susInf;
+    }
+
+    public static Perfil cursorToPerfil(Cursor cursor) {
+        Perfil perfil = new Perfil();
+
+        perfil.setId(cursor.getLong(cursor.getColumnIndex(BdTablePerfil._ID)));
+        perfil.setNome(cursor.getString(cursor.getColumnIndex(BdTablePerfil.CAMPO_NOME)));
+        perfil.setDataNascimento(cursor.getString(cursor.getColumnIndex(BdTablePerfil.CAMPO_DATA_NASCIMENTO)));
+        perfil.setSexo(cursor.getString(cursor.getColumnIndex(BdTablePerfil.CAMPO_SEXO)));
+        perfil.setAltura(cursor.getInt(cursor.getColumnIndex(BdTablePerfil.CAMPO_ALTURA)));
+        perfil.setPeso(cursor.getFloat(cursor.getColumnIndex(BdTablePerfil.CAMPO_PESO)));
+        perfil.setTipoSangue(cursor.getString(cursor.getColumnIndex(BdTablePerfil.CAMPO_TIPO_SANGUE)));
+
+        return perfil;
+    }
+
+    public static Sintoma cursorToSintoma(Cursor cursor){
+        Sintoma sintoma = new Sintoma();
+
+        sintoma.setId(cursor.getLong(cursor.getColumnIndex(BdTableSintoma._ID)));
+        sintoma.setData(cursor.getString(cursor.getColumnIndex(BdTableSintoma.CAMPO_DATA)));
+        sintoma.setDoresCabeca(cursor.getString(cursor.getColumnIndex(BdTableSintoma.CAMPO_DORES_CABECA)));
+        sintoma.setDoresMusculares(cursor.getString(cursor.getColumnIndex(BdTableSintoma.CAMPO_DORES_MUSCULARES)));
+        sintoma.setCansaco(cursor.getString(cursor.getColumnIndex(BdTableSintoma.CAMPO_CANSACO)));
+        sintoma.setDoresGarganta(cursor.getString(cursor.getColumnIndex(BdTableSintoma.CAMPO_DORES_GARGANTA)));
+        sintoma.setTosse(cursor.getString(cursor.getColumnIndex(BdTableSintoma.CAMPO_TOSSE)));
+        sintoma.setTemperatura(cursor.getFloat(cursor.getColumnIndex(BdTableSintoma.CAMPO_TEMPERATURA)));
+        sintoma.setRespiracao(cursor.getString(cursor.getColumnIndex(BdTableSintoma.CAMPO_RESPIRACAO)));
+        sintoma.setCorrimentoNasal(cursor.getString(cursor.getColumnIndex(BdTableSintoma.CAMPO_CORRIMENTO_NASAL)));
+        sintoma.setNomePerfil(cursor.getString(cursor.getColumnIndex(BdTableSintoma.CAMPO_PERFIL)));
+        sintoma.setIdPerfil(cursor.getLong(cursor.getColumnIndex(BdTableSintoma.CAMPO_ID_PERFIL)));
+
+        return sintoma;
+    }
+
+    public static SusInf cursorToSusInf(Cursor cursor){
+        SusInf susInf = new SusInf();
+
+        susInf.setId(cursor.getLong(cursor.getColumnIndex(BdTableSusInf._ID)));
+        susInf.setNomeSusInf(cursor.getString(cursor.getColumnIndex(BdTableSusInf.CAMPO_NOME_SUS_INF)));
+        susInf.setDataNascimento(cursor.getString(cursor.getColumnIndex(BdTableSusInf.CAMPO_DATA_NASCIMENTO)));
+        susInf.setDataInfecao(cursor.getString(cursor.getColumnIndex(BdTableSusInf.CAMPO_DATA_INFECAO)));
+        susInf.setNomePerfil(cursor.getString(cursor.getColumnIndex(BdTableSusInf.CAMPO_PERFIL)));
+        susInf.setIdPerfil(cursor.getLong(cursor.getColumnIndex(BdTableSusInf.CAMPO_ID_PERFIL)));
+        
         return susInf;
     }
 }
