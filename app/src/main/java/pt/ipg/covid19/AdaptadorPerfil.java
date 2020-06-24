ed4de6ce57629e6d29c1ab49d32d13e323156636
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-24 15:55:20 +0100

    AdaptadorPerfil criado

---
+++ app/src/main/java/pt/ipg/covid19/AdaptadorPerfil.java
@@ -0,0 +1,116 @@
+package pt.ipg.covid19;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdaptadorPerfil extends RecyclerView.Adapter<AdaptadorPerfil.ViewHolderPerfil>{
+    private final Context context;
+    private Cursor cursor = null;
+
+    public void setCursor(Cursor cursor) {
+        if (cursor != this.cursor) {
+            this.cursor = cursor;
+            notifyDataSetChanged();
+        }
+    }
+
+    public AdaptadorPerfil(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderPerfil onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemPerfil = LayoutInflater.from(context).inflate(R.layout.item_perfil, parent, false);
+
+        return new ViewHolderPerfil(itemPerfil);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderPerfil holder, int position) {
+        cursor.moveToPosition(position);
+        Perfil perfil = Converte.cursorToPerfil(cursor);
+        holder.setPerfil(perfil);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (cursor == null) {
+            return 0;
+        }
+        return cursor.getCount();
+    }
+
+    public Perfil getPerfilSelecionado(){
+        if (viewHolderPerfilSelecionado == null) return null;
+
+        return viewHolderPerfilSelecionado.perfil;
+    }
+
+    private ViewHolderPerfil viewHolderPerfilSelecionado = null;
+
+    public class ViewHolderPerfil extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private Perfil perfil = null;
+
+        private final TextView textViewNome;
+        private final TextView textViewDataNascimento;
+        private final TextView textViewSexo;
+        private final TextView textViewAltura;
+        private final TextView textViewPeso;
+        private final TextView textViewTipoSangue;
+
+
+        public ViewHolderPerfil(@NonNull View itemView) {
+            super(itemView);
+
+            textViewNome = (TextView) itemView.findViewById(R.id.textViewItemNome);
+            textViewDataNascimento = (TextView) itemView.findViewById(R.id.textViewItemDataNascimento);
+            textViewSexo = (TextView) itemView.findViewById(R.id.textViewItemSexo);
+            textViewAltura = (TextView) itemView.findViewById(R.id.textViewItemAltura);
+            textViewPeso = (TextView) itemView.findViewById(R.id.textViewItemPeso);
+            textViewTipoSangue = (TextView) itemView.findViewById(R.id.textViewItemTipoSangue);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void setPerfil(Perfil perfil) {
+
+            this.perfil = perfil;
+
+            textViewNome.setText(perfil.getNome());
+            textViewDataNascimento.setText(perfil.getDataNascimento());
+            textViewSexo.setText(perfil.getSexo());
+            textViewAltura.setText((int)perfil.getAltura());
+            textViewPeso.setText((int) perfil.getPeso());
+            textViewTipoSangue.setText(perfil.getTipoSangue());
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (viewHolderPerfilSelecionado != null) {
+                viewHolderPerfilSelecionado.desSeleciona();
+            }
+
+            viewHolderPerfilSelecionado = this;
+
+            ((activity_perfil) context).atualizaOpcoesMenu();
+            seleciona();
+        }
+
+        private void seleciona() {
+            itemView.setBackgroundResource(R.color.colorPrimary);
+        }
+
+        private void desSeleciona() {
+            itemView.setBackgroundResource(android.R.color.darker_gray);
+        }
+    }
+}
--- app/src/main/java/pt/ipg/covid19/activity_perfil.java
+++ app/src/main/java/pt/ipg/covid19/activity_perfil.java
@@ -1,65 +1,97 @@
 package pt.ipg.covid19;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class activity_perfil extends AppCompatActivity {
+public class activity_perfil extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    public static final String ID_PERFIL = "ID_PERFIL";
+
+    private AdaptadorPerfil adaptadorPerfil;
+    private RecyclerView recyclerViewPerfil;
+    private Menu menu;
+
+    public static final int ID_CURSOR_LOADER_PERFIL = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
-        Intent intent = getIntent();
-        //data Nascimento
-        String data = intent.getStringExtra("covid19_data");
-        TextView dataNascimento = (TextView) findViewById(R.id.textViewNascimento);
-        dataNascimento.setText(data);
-        //Sexo
-        String sexo = intent.getStringExtra("covid19_sexo");
-        TextView Sexo = (TextView) findViewById(R.id.textViewSexo);
-        Sexo.setText(sexo);
-        //altura
-        String altura = intent.getStringExtra("covid19_altura");
-        TextView AlturaText = (TextView) findViewById(R.id.textViewAltura);
-        AlturaText.setText(altura);
-        //peso
-        String peso = intent.getStringExtra("covid19_peso");
-        TextView PesoText = (TextView) findViewById(R.id.textViewPeso);
-        PesoText.setText(peso);
-        //Sangue
-        String sangue = intent.getStringExtra("covid19_sangue");
-        TextView TipoSangue = (TextView) findViewById(R.id.textViewTipoSangue);
-        TipoSangue.setText(sangue);
+        getSupportLoaderManager().initLoader(ID_CURSOR_LOADER_PERFIL, null, this);
+        recyclerViewPerfil = (RecyclerView) findViewById(R.id.recyclerViewPerfil);
+        adaptadorPerfil = new AdaptadorPerfil(this);
+        recyclerViewPerfil.setAdapter(adaptadorPerfil);
+        recyclerViewPerfil.setLayoutManager(new LinearLayoutManager(this));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        adaptadorPerfil.setCursor(null);
     }
 
-    public void onClickEditarPerfil(View view){
-        Intent IntentEditarPerfil = new Intent(this, activity_perfil_editar.class);
-        startActivity(IntentEditarPerfil);
+    @Override
+    protected void onResume() {
+        getSupportLoaderManager().restartLoader(ID_CURSOR_LOADER_PERFIL, null, this);
+        super.onResume();
     }
 
-    public void onClickApagarPerfil(View view){
-        //apaga data
-        TextView dataNascimento = (TextView) findViewById(R.id.textViewNascimento);
-        dataNascimento.setText("");
-        //apaga sexo
-        TextView Sexo = (TextView) findViewById(R.id.textViewSexo);
-        Sexo.setText("");
-        //apaga altura
-        TextView AlturaText = (TextView) findViewById(R.id.textViewAltura);
-        AlturaText.setText("");
-        //apaga peso
-        TextView PesoText = (TextView) findViewById(R.id.textViewPeso);
-        PesoText.setText("");
-        //apaga sangue
-        TextView TipoSangue = (TextView) findViewById(R.id.textViewTipoSangue);
-        TipoSangue.setText("");
-        //toast para dizer que foi apagado
-        Toast.makeText(this, ("Apagado"),Toast.LENGTH_LONG).show();
+    public void atualizaOpcoesMenu() {
+        Perfil perfilModel = adaptadorPerfil.getPerfilSelecionado();
+
+        boolean mostraAlterarEliminar = (perfilModel != null);
+        menu.findItem(R.id.action_moreEdit).setVisible(mostraAlterarEliminar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_perfil, menu);
+
+        this.menu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_moreEdit) {
+            Intent intent = new Intent(this, activity_perfil_editar.class);
+
+            intent.putExtra(ID_PERFIL, adaptadorPerfil.getPerfilSelecionado().getId());
+
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return new CursorLoader(this, CovidContentProvider.ENDERECO_PERFIL,
+                BdTablePerfil.TODOS_CAMPOS, null, null, BdTablePerfil.CAMPO_NOME);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        adaptadorPerfil.setCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
