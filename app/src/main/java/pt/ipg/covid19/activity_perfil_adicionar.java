2250e0f92c98546a987a5872fed7a2c071d1d3ae
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-25 19:05:53 +0100

    layout para activity perfil add criada

---
+++ app/src/main/java/pt/ipg/covid19/activity_perfil_adicionar.java
@@ -0,0 +1,14 @@
+package pt.ipg.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_perfil_adicionar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_adicionar);
+    }
+}
