5f62c285952b9af29516d2c540ab7c7a1b4b8fa7
Rodrigo Almeida <32521408+rpa299@users.noreply.github.com>
2020-06-26 15:09:05 +0100

    Alterar Perfil feito

--- app/src/main/java/pt/ipg/covid19/Perfil.java
+++ app/src/main/java/pt/ipg/covid19/Perfil.java
@@ -1,5 +1,8 @@
 package pt.ipg.covid19;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+
 public class Perfil {
     private long id = -1;
     private String nome;
@@ -63,5 +66,53 @@ public class Perfil {
 
     public void setTipoSangue(String tipoSangue) {
         this.tipoSangue = tipoSangue;
+    }
+
+    public ContentValues getContentValues(){
+        ContentValues valores = new ContentValues();
+        valores.put(BdTablePerfil.CAMPO_NOME, nome);
+        valores.put(BdTablePerfil.CAMPO_DATA_NASCIMENTO, dataNascimento);
+        valores.put(BdTablePerfil.CAMPO_SEXO,sexo);
+        valores.put(BdTablePerfil.CAMPO_ALTURA,altura);
+        valores.put(BdTablePerfil.CAMPO_PESO,peso);
+        valores.put(BdTablePerfil.CAMPO_TIPO_SANGUE,tipoSangue);
+        return valores;
+    }
+
+    public static Perfil fromCursor(Cursor cursor){
+
+        long id = cursor.getLong(
+                cursor.getColumnIndex(BdTablePerfil._ID)
+        );
+        String Nome = cursor.getString(
+                cursor.getColumnIndex(BdTablePerfil.CAMPO_NOME)
+        );
+        String DataNascimento = cursor.getString(
+                cursor.getColumnIndex(BdTablePerfil.CAMPO_DATA_NASCIMENTO)
+        );
+        String Sexo = cursor.getString(
+                cursor.getColumnIndex(BdTablePerfil.CAMPO_SEXO)
+        );
+        int Altura = cursor.getInt(
+                cursor.getColumnIndex(BdTablePerfil.CAMPO_ALTURA)
+        );
+        int Peso = cursor.getInt(
+                cursor.getColumnIndex(BdTablePerfil.CAMPO_PESO)
+        );
+        String TipoSangue = cursor.getString(
+                cursor.getColumnIndex(BdTablePerfil.CAMPO_TIPO_SANGUE)
+        );
+
+        Perfil perfil = new Perfil();
+
+        perfil.setId(id);
+        perfil.setNome(Nome);
+        perfil.setDataNascimento(DataNascimento);
+        perfil.setSexo(Sexo);
+        perfil.setAltura(Altura);
+        perfil.setPeso(Peso);
+        perfil.setTipoSangue(TipoSangue);
+
+        return perfil;
     }
 }
--- app/src/main/java/pt/ipg/covid19/activity_perfil_editar.java
+++ app/src/main/java/pt/ipg/covid19/activity_perfil_editar.java
@@ -5,10 +5,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
 import androidx.loader.content.Loader;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
@@ -27,17 +30,22 @@ import java.text.SimpleDateFormat;
 import java.time.Year;
 import java.util.Calendar;
 
-public class activity_perfil_editar extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class activity_perfil_editar extends AppCompatActivity implements AdapterView.OnItemSelectedListener, LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int ID_CURSOR_LOADER_PERFIL = 0;
+    private Uri enderecoPerfilEditar;
+
     TextView dataNascimento;
     TextInputEditText editTextAlturaEditar;
     TextInputEditText editTextPesoEditar;
     TextInputEditText editTextNomeEditar;
+    TextView textViewDataNascimento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_editar);
 
+        textViewDataNascimento = findViewById(R.id.textViewDataNascimento);
         editTextNomeEditar = (TextInputEditText) findViewById(R.id.editTextNomeEditPerfil);
         editTextAlturaEditar =(TextInputEditText) findViewById(R.id.editTextAlturaEditar);
         editTextPesoEditar = (TextInputEditText) findViewById(R.id.editTextPesoEditar);
@@ -53,6 +61,36 @@ public class activity_perfil_editar extends AppCompatActivity implements Adapter
         adapterSangue.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerSangue.setAdapter(adapterSangue);
         spinnerSangue.setOnItemSelectedListener(this);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportLoaderManager().initLoader(ID_CURSOR_LOADER_PERFIL, null, this);
+        Intent intent = getIntent();
+        Long idPerfil = intent.getLongExtra(activity_perfil.ID_PERFIL,-1);
+        if(idPerfil == -1){
+            Toast.makeText(this,R.string.Erro,Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        enderecoPerfilEditar = Uri.withAppendedPath(CovidContentProvider.ENDERECO_PERFIL,String.valueOf(idPerfil));
+
+        Cursor cursor = getContentResolver().query(enderecoPerfilEditar, BdTablePerfil.TODOS_CAMPOS, null, null, null);
+
+        if(!cursor.moveToNext()){
+            Toast.makeText(this,R.string.Erro, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        Perfil perfil = Perfil.fromCursor(cursor);
+
+        editTextNomeEditar.setText(perfil.getNome());
+        textViewDataNascimento.setText(perfil.getDataNascimento());
+        spinner.setSelection(getSpinnerSexo(spinner,perfil.getSexo()));
+        editTextAlturaEditar.setText(""+perfil.getAltura());
+        editTextPesoEditar.setText(""+perfil.getPeso());
+        spinnerSangue.setSelection(getSpinnerSangue(spinnerSangue,perfil.getTipoSangue()));
     }
 
     public void GuardarPerfil(View view){
@@ -84,9 +122,23 @@ public class activity_perfil_editar extends AppCompatActivity implements Adapter
         datePickerDialog.show();
     }
 
-    public void onClickPerfilEditar(View view){
-        Intent IntentPerfilEditar = new Intent(this, activity_perfil_editar.class);
-        startActivity(IntentPerfilEditar);
+    private int getSpinnerSexo(Spinner spinner, String sexo){
+        int index = 0;
+        for(int i = 0; i<spinner.getCount();i++){
+            if(spinner.getItemAtPosition(i).equals(sexo)){
+                index = i;
+            }
+        }
+        return index;
+    }
+    public int getSpinnerSangue(Spinner spinner, String sangue){
+        int index = 0;
+        for(int i = 0; i<spinner.getCount();i++){
+            if(spinner.getItemAtPosition(i).equals(sangue)){
+                index = i;
+            }
+        }
+        return index;
     }
 
     @Override
@@ -100,35 +152,84 @@ public class activity_perfil_editar extends AppCompatActivity implements Adapter
     }
 
     public void validarCampos(){
-        TextView mensagemNome = (TextInputEditText) findViewById(R.id.editTextNomeEditPerfil);
-        TextInputEditText mensagemAlturaEditar = (TextInputEditText) findViewById(R.id.editTextAlturaEditar);
-        TextInputEditText mensagemPesoEditar = (TextInputEditText) findViewById(R.id.editTextPesoEditar);
+        TextView mensagemNomeEdit = (TextInputEditText) findViewById(R.id.editTextNomeEditPerfil);
+        TextView mensagemDataEdit = (TextView) findViewById(R.id.textViewDataNascimento);
+        Spinner spinnerSexoEdit = findViewById(R.id.spinnerSexoEditPerfil);
+        TextInputEditText mensagemAlturaEdit = (TextInputEditText) findViewById(R.id.editTextAlturaEditar);
+        TextInputEditText mensagemPesoEdit = (TextInputEditText) findViewById(R.id.editTextPesoEditar);
+        Spinner spinnerSangueEdit = findViewById(R.id.spinnerTipoSangueEditPerfil);
 
         //mete os valores em strings
-        String nome =mensagemNome.getText().toString();
-        String altura = mensagemAlturaEditar.getText().toString();
-        String peso = mensagemPesoEditar.getText().toString();
+        String nome =mensagemNomeEdit.getText().toString();
+        String data = mensagemDataEdit.getText().toString();
+        String sexo = spinnerSexoEdit.getSelectedItem().toString();
+        String altura = mensagemAlturaEdit.getText().toString();
+        String peso = mensagemPesoEdit.getText().toString();
+        String sangue = spinnerSangueEdit.getSelectedItem().toString();
 
         //validação
-        if(nome.trim().length() == 0){
-            mensagemNome.setError(getString(R.string.obrigatorio));
-            mensagemNome.requestFocus();
+        if(nome.trim().isEmpty()){
+            mensagemNomeEdit.setError(getString(R.string.obrigatorio));
+            mensagemNomeEdit.requestFocus();
             return;
         }
-        if(altura.trim().length() == 0){
-            mensagemAlturaEditar.setError(getString(R.string.obrigatorio));
-            mensagemAlturaEditar.requestFocus();
+        if(altura.trim().isEmpty()){
+            mensagemAlturaEdit.setError(getString(R.string.obrigatorio));
+            mensagemAlturaEdit.requestFocus();
             return;
         }
-        if(peso.trim().length() == 0){
-            mensagemPesoEditar.setError(getString(R.string.obrigatorio));
-            mensagemPesoEditar.requestFocus();
+        if(peso.trim().isEmpty()){
+            mensagemPesoEdit.setError(getString(R.string.obrigatorio));
+            mensagemPesoEdit.requestFocus();
             return;
+        }
+        String nomeAlterado =mensagemNomeEdit.getText().toString();
+        String dataAlterado = mensagemDataEdit.getText().toString();
+        String sexoAlterado = spinnerSexoEdit.getSelectedItem().toString();
+        Integer alturaInt = Integer.parseInt(altura);
+        Integer pesoINT = Integer.parseInt(peso);
+        String sangueAlterado = spinnerSangueEdit.getSelectedItem().toString();
+
+        //guardar
+        Perfil perfil = new Perfil();
+        perfil.setNome(nomeAlterado);
+        perfil.setDataNascimento(dataAlterado);
+        perfil.setSexo(sexoAlterado);
+        perfil.setAltura(alturaInt);
+        perfil.setPeso(pesoINT);
+        perfil.setTipoSangue(sangueAlterado);
+
+        try {
+            getContentResolver().update( enderecoPerfilEditar, perfil.getContentValues(), null, null);
+
+            Toast.makeText(this, (R.string.Sucesso), Toast.LENGTH_SHORT).show();
+            finish();
+
+        } catch (Exception e) {
+            Toast.makeText(this,(R.string.Erro), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
         }
     }
 
     public void cancelar(View view){
         finish();
         Toast.makeText(this, ("Cancelado"),Toast.LENGTH_LONG).show();
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return new CursorLoader(this, CovidContentProvider.ENDERECO_PERFIL, BdTablePerfil.TODOS_CAMPOS,
+                null, null, BdTablePerfil.CAMPO_NOME);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
     }
 }
